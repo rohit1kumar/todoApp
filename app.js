a359d0ffc5b83c19b470cc132dd4b3c6b1ed0cef
Rohit Kumar <sonu12345kumar12345@gmail.com>
2022-01-31 12:38:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({ name: "welcome to your todolist" });
+const item2 = new Item({ name: "Hit the + button to add a new item." });
+const item3 = new Item({ name: "<-- Hit this to delete an item." });
+
+const defaultItems = [item1, item2, item3];
+
+app.get("/", function (req, res) {
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully savevd default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
+
+});
+
+app.post("/", function (req, res) {
+  const itemName = _.capitalize(req.body.newItem);
+  const item = new Item({
+    name: itemName
+  });
+  item.save();
+  res.redirect("/");
+});
+
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (!err) {
+      console.log("successfully deleted checked item");
+      res.redirect("/");
+    }
+  })
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started on port 3000");
+});
